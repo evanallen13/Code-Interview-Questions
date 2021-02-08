e0c111341631c2669343a9481258d9d1183933dd
Evan <evanallen13.gmail.com>
2021-02-08 13:53:39 -0800

    Two Sums

---
+++ LeetCode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// TWO SUMS ------------------------
+            //int[] nums = new int[] { 3, 2, 4 };
+            //int target = 6;
+            //int[] result = TwoSum.twoSum(nums, target);
+            //Console.WriteLine($"[{result[0]},{result[1]}]");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCode/TwoSums.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+
+    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    //You can return the answer in any order.
+    class TwoSum
+    {
+        public static int[] twoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Boolean isReturnResult = false;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                int num = nums[j];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (i != j)
+                    {
+                        if (num + nums[i] == target)
+                        {
+                            result[0] = j;
+                            result[1] = i;
+                            isReturnResult = true;
+                            break;
+                        }
+                    }
+
+                }
+                if (isReturnResult)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
