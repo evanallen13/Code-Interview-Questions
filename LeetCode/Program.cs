9e7a66c596106c2b14e856fc6b62b16f0bbc98c5
Evan <evanallen13.gmail.com>
2021-02-08 18:59:29 -0800

    LongestCommonPrefix

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,9 +12,23 @@ namespace LeetCode
             //int[] result = TwoSum.twoSum(nums, target);
             //Console.WriteLine($"[{result[0]},{result[1]}]");
 
-            Console.WriteLine(IsPalindrome.isPalindrome(-121));
+            //Console.WriteLine(ReverseInteger.Reverse(1534236469));
+
+            //Console.WriteLine(IsPalindrome.isPalindrome(232));
+
+            //Console.WriteLine(LongestPalidrome.longestPalindrome("flsuqzhtcahnyickkgtfnlyzwjuiwqiexthpzvcweqzeqpmqwkydhsfipcdrsjkefehhesubkirhalgnevjugfohwnlhbjfewiunlgmomxkafuuokesvfmcnvseixkkzekuinmcbmttzgsqeqbrtlwyqgiquyylaswlgfflrezaxtjobltcnpjsaslyviviosxorjsfncqirsjpkgajkfpoxxmvsyynbbovieoothpjgncfwcvpkvjcmrcuoronrfjcppbisqbzkgpnycqljpjlgeciaqrnqyxzedzkqpqsszovkgtcgxqgkflpmrikksaupukdvkzbltvefitdegnlmzeirotrfeaueqpzppnsjpspgomyezrlxsqlfcjrkglyvzvqakhtvfmeootbtbwfhqucbnuwznigoyatvkocqmbtqghybwrhmyvvuchjpvjckiryvjfxabezchynfxnpqaeampvaapgmvoylyutymdhvhqfmrlmzkhuhupizqiujpwzarnszrexpvgdmtoxvjygjpmiadzdcxtggwamkbwrkeplesupagievwsaaletcuxtpsxmbmeztcylsjxvhzrqizdmgjfyftpzpgxateopwvynljzffszkzzqgofdlwyknqfruhdkvmvrrjpijcjomnrjjubfccaypkpfokohvkqndptciqqiscvmpozlyyrwobeuazsawtimnawquogrohcrnmexiwvjxgwhmtpykqlcfacuadyhaotmmxevqwarppknoxthsmrrknu"));
+
+            string[] arr = new string[] { "dog", "racecar", "car" };
+            Console.WriteLine(LongestCommonPrefix.longestCommonPrefix(arr));
+
+            arr = new string[] {"Ab", "A"};
+            Console.WriteLine(LongestCommonPrefix.longestCommonPrefix(arr));
+
+            arr = new string[] { "dog"};
+            Console.WriteLine(LongestCommonPrefix.longestCommonPrefix(arr));
 
             Console.ReadLine();
         }
+
     }
 }
