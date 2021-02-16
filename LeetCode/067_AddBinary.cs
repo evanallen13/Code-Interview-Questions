c752afb060f16d2f05c7880f1e13032e80a249fd
evanallen13 <evanallen13@gmail.com>
2021-02-15 19:47:01 -0800

    add 66 Plus One

---
+++ LeetCode/067_AddBinary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class _067_AddBinary
+    {
+        public string AddBinary(string a, string b)
+        {
+
+        }
+    }
+}
