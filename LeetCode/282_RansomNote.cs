bb074ee3599d70d26e2a3cc3e06aacf329823f80
evanallen13 <evanallen13@gmail.com>
2021-02-16 19:21:33 -0800

    start 383

---
+++ LeetCode/282_RansomNote.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class _282_RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            bool result = false;
+
+            List<char> magazineList = new List<char>();
+            magazineList.AddRange(magazineList);
+
+
+            char[] ransomNoteArr = ransomNote.ToCharArray();
+
+
+            foreach(char chr in ransomNoteArr)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
