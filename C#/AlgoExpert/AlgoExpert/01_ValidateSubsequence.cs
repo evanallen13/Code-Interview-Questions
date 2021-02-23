eabfc2550186a2ffacc615088e45035c83dc1a12
Evanallen13 <evanallen13@gmail.com>
2021-02-23 14:04:23 -0800

    add AlgoExpert

---
+++ C#/AlgoExpert/AlgoExpert/01_ValidateSubsequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoExpert
+{
+    class _01_ValidateSubsequence
+    {
+		public static bool IsValidSubsequence()
+		{
+			List<int> array = new List<int>() { 1, 1, 1, 1, 1 };
+			List<int> sequence = new List<int>() { 1, 1, 1, 1, 1 };
+
+			int lastIndex = -1;
+
+			foreach(var num in sequence)
+            {
+				var index = array.IndexOf(num);
+				if (index == -1)
+					return false;
+
+				if (lastIndex < index)
+					lastIndex = index;
+            }
+
+
+			return true;
+		}
+	}
+}
---
+++ C#/AlgoExpert/AlgoExpert/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlgoExpert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _01_ValidateSubsequence.IsValidSubsequence();
+            Console.ReadLine();
+        }
+    }
+}
