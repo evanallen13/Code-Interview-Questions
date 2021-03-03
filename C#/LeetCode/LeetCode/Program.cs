4893a1a74ec4836fbfcb8fb62c8aadd747ebcacc
evanallen13 <evanallen13@gmail.com>
2021-03-03 10:34:10 -0800

    add 104 Max Depth

--- C#/LeetCode/LeetCode/Program.cs
+++ C#/LeetCode/LeetCode/Program.cs
@@ -8,7 +8,19 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(_172_FactorialTrailingZero.TrailingZeroes(30));
+            TreeNode root = new TreeNode(3);
+            // 
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(20);
+            //
+            //root.left.left = new TreeNode(3);
+            //root.left.right = new TreeNode(4);
+            root.right.left = new TreeNode(15);
+            root.right.right = new TreeNode(7);
+            // 
+
+
+            Console.WriteLine(_104_MaximumDepthOfABinaryTree.MaxDepth(root));
 
             Console.ReadLine();
 
