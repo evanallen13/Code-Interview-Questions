2f49b6d7c5b54cfa6fec8b8490892f9ea39aef74
evanallen13 <evanallen13@gmail.com>
2021-03-15 19:29:53 -0700

    Add 160

--- C#/LeetCode/LeetCode/Program.cs
+++ C#/LeetCode/LeetCode/Program.cs
@@ -9,8 +9,21 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            ListNode headA = new ListNode(4);
+            headA.next = new ListNode(1);
+            headA.next.next = new ListNode(8);
+            headA.next.next.next = new ListNode(4);
+            headA.next.next.next.next = new ListNode(5);
 
-            _342_PowerOfFour.IsPowerOfFour(10);
+            ListNode headB = new ListNode(5);
+            headB.next = new ListNode(6);
+            headB.next.next = new ListNode(1);
+            headB.next.next.next = new ListNode(8);
+            headB.next.next.next.next = new ListNode(4);
+            headB.next.next.next.next.next = new ListNode(5);
+
+
+            _0160_Intersection_of_Two_Linked_Lists.GetIntersectionNode(headA, headB);
 
             Console.ReadLine();
 
