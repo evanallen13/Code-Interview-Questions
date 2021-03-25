feb72e8ad4bbf2bdf0031b87e3bb79d8c981656f
evanallen13 <evanallen13@gmail.com>
2021-03-24 19:57:10 -0700

    add .net 5

---
+++ C#/LeetCode/DataStuctures/ListNode_Load.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.DataStuctures
+{
+    class ListNode_Load
+    {
+        public static ListNode Load(int[] array)
+        {
+            ListNode node = null;
+
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                int val = array[i];
+                var dummy = new ListNode(val);
+                dummy.next = node;
+                node = dummy;
+
+            }
+
+            return node;
+        }
+    }
+}
---
+++ C#/LeetCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int[] arr = new int[]{6,5,2};
+
+            var node = LeetCode.DataStuctures.ListNode_Load.Load(arr);
+
+
+            var dummy =  _203_RemoveLinkedListElement.RemoveElements(node, 5);
+
+            while(dummy != null){
+                Console.WriteLine(dummy.val);
+                dummy = dummy.next;
+            }
+            
+        }
+    }
+}
