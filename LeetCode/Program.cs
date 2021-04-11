a727961f1b75ef8e78a5420f442cdea8d85f29f5
evanallen13 <evanallen13@gmail.com>
2021-04-11 10:09:41 -0700

    convert to .Net 5

---
+++ LeetCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LeetCode.DataStuctures;
+using LeetCode.Questions;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int[] nums = new int[]{1,2,3,4,5};
+            int target = 5;
+
+           int[] res = _001_TwoSums2.twoSum(nums, target);
+
+           foreach(int i in res){
+               Console.Write(i);
+           }
+        }
+    }
+}
---
+++ LeetCode/Questions/034_FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Questions
+{
+    class _034_FindFirstAndLastPositionOfElementInSortedArray
+    {
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            if (nums.Length == 0) return new int[] { -1, -1 };
+
+            int first = -1;
+            int last = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    if (first != -1)
+                        last = i;
+                    else
+                    {
+                        first = i;
+                        last = i;
+                    }
+                }
+            }
+
+            return new int[] { first, last };
+        }
+    }
+}
